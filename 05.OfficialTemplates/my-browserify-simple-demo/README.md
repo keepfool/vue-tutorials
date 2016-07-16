41f143abf41865805691ac7259974606da538682
keepfool <crmug@outlook.com>
2016-07-16 21:24:12 +0800

    Vue.js browserify-simple template tutorial

---
+++ 05.OfficialTemplates/my-browserify-simple-demo/README.md
@@ -0,0 +1,18 @@
+# my-browserify-simple-demo
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For more information see the [docs for vueify](https://github.com/vuejs/vueify).
---
+++ 05.OfficialTemplates/my-browserify-simple-demo/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="app">
+		<h2>This is a grid component built with vue cli browserify-simple scaffolding!</h1>
+		<div id="searchBar">
+			Search <input type="text" v-model="searchQuery" />
+		</div>
+		<simple-grid :data="gridData" :columns="gridColumns" :filter-key="searchQuery" :sort-order="sortOrder">
+		</simple-grid>
+	</div>
+</template>
+
+<script>
+	import simpleGrid from './components/SimpleGrid.vue'
+
+	export default {
+		data() {
+			return {
+				searchQuery: '',
+				// order > 0：正序，order < 0：倒序
+				sortOrder: {
+					column: 'name',
+					order: 1
+				},
+				gridColumns: ['name', 'power'],
+				gridData: [{
+					name: 'Chuck Norris',
+					power: Infinity
+				}, {
+					name: 'Bruce Lee',
+					power: 9000
+				}, {
+					name: 'Jackie Chan',
+					power: 7000
+				}, {
+					name: 'Jet Li',
+					power: 8000
+				}]
+			}
+		}, components: {
+			simpleGrid
+		}
+	}
+</script>
+<style>
+	body {
+		font-family: Helvetica, sans-serif;
+		color: #333;
+	}
+</style>
