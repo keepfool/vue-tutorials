ebdce6c4a381c5971d57b48ada2dbb8ca3f89560
Ali raza <72508498+Aliraza2686@users.noreply.github.com>
2021-09-14 09:10:56 -0700

    Created Help.vue

---
+++ 06.Router/demo01/src/components/Help.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+     <h1>This is help page</h1>
+     <router-link to="/home">Go back to home page</router-link>
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+.container{
+background-color : #f2f2f2;
+padding : 10px;
+}
+</style>
